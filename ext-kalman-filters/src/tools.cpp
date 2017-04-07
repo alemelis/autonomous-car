ea13d0d88513da97ba676ebf69da38d2528a6e7c
Alessandro Melis <alessandro_melis@rocketmail.com>
2017-04-07 10:06:29 +0100

    type:complete Completed RMSE function

--- ext-kalman-filters/src/tools.cpp
+++ ext-kalman-filters/src/tools.cpp
@@ -11,20 +11,46 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+  VectorXd rmse(4);
+  rmse << 0,0,0,0;
+
+  // check the validity of the following inputs:
+  //  * the estimation vector size should not be zero
+  if(estimations.size()==0){
+      cout << "ERROR: zero size estimation vector";
+      return rmse;
+  }
+  //  * the estimation vector size should equal ground truth vector size
+  // ... your code here
+  if(estimations.size() != ground_truth.size()){
+      cout << "ERROR: estimation size different than ground truth size";
+      return rmse;
+  }
+
+  //accumulate squared residuals
+  for(int i=0; i < estimations.size(); ++i){
+        // ... your code here
+        VectorXd residual = estimations[i] - ground_truth[i];
+        residual = residual.array()*residual.array();
+  	rmse += residual;
+  }
+
+  //calculate the mean
+  // ... your code here
+  rmse = rmse.array()/estimations.size();
+
+  //calculate the squared root
+  // ... your code here
+  rmse = rmse.array().sqrt();
+
+  //return the result
+  return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-  TODO:
-    * Calculate a Jacobian here.
-  */
 
   MatrixXd Hj(3,4);
-  
+
 	//recover state parameters
 	float px = x_state(0);
 	float py = x_state(1);
