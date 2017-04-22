c78f73a5998dffdfed3fb277822631827a94a270
AM <alessandro_melis@rocketmail.com>
2017-04-22 10:19:56 +0100

    Completed initialisation

--- unsc-kalman-filter/src/ukf.cpp
+++ unsc-kalman-filter/src/ukf.cpp
@@ -25,10 +25,10 @@ UKF::UKF() {
   P_ = MatrixXd(5, 5);
 
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 30;
+  std_a_ = 3;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 30;
+  std_yawdd_ = 2;
 
   // Laser measurement noise standard deviation position1 in m
   std_laspx_ = 0.15;
@@ -40,10 +40,10 @@ UKF::UKF() {
   std_radr_ = 0.3;
 
   // Radar measurement noise standard deviation angle in rad
-  std_radphi_ = 0.03;
+  std_radphi_ = 0.0175;
 
   // Radar measurement noise standard deviation radius change in m/s
-  std_radrd_ = 0.3;
+  std_radrd_ = 0.1;
 
   /**
   TODO:
@@ -52,6 +52,21 @@ UKF::UKF() {
 
   Hint: one or more values initialized above might be wildly off...
   */
+  is_initialized_ = True;
+
+  time_us_ = 0; //???
+
+  n_x_ = 5;
+  n_aug_ = 7;
+
+  Xsig_pred_ = MatrixXd(n_x_, 2*n_aug_ + 1);
+
+  weights_ = VectorXd(2*n_aug_ + 1);
+
+  lambda_ = 3 - n_aug_;
+
+  NIS_radar_ = 0;
+  NIS_laser_ = 0;
 }
 
 UKF::~UKF() {}
